46c135f34a0f07d56161497c1b107f54656a3c9f
kellton <nayanmeher16@gmail.com>
2019-09-25 18:47:42 +0530

    Initial commit

---
+++ app/src/main/java/com/kellton/mvvmtest1/MainActivity.java
@@ -0,0 +1,78 @@
+package com.kellton.mvvmtest1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
+
+    private ArrayList<String> imageUrl = new ArrayList<>();
+    private ArrayList<String> imageName = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        Log.d(TAG, "onCreate: started");
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initializeList();
+        initRecyclerView();
+    }
+
+    private void initializeList(){
+        imageName.add("Puri Tample");
+        imageUrl.add("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQVLhAZ3XH49jfbPQYOFSXt7pLn-xXHxRctTwVMfTw8Jkr3t-qo");
+
+        imageName.add("Nrusinghnath");
+        imageUrl.add("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQAIO1GTIjRTlmyWSYTe4MGe_5azdNxlpgnbNDhwN_narqzxqC-");
+
+        imageName.add("Konark Tample");
+        imageUrl.add("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTbJreNEzntqLYCUzg3AiNubdNf_WNIuDZN4XuvUEV3F7SoK4en");
+
+        imageName.add("Golden Tample");
+        imageUrl.add("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQLinSWvdW7LeZtvmvQ7Qaq0wEg-GYIJ4DE3kF_af1t1z-vm1pn");
+
+        imageName.add("Taj Mahal");
+        imageUrl.add("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQj-y51EfuVHY5vI2crccf1lRkTJs0vxjpuz4KOZhIYnDDlqIjJ");
+
+        imageName.add("Hampi");
+        imageUrl.add("https://ihplb.b-cdn.net/wp-content/uploads/2018/06/hampi.jpg");
+
+        imageName.add("Qutub Minar");
+        imageUrl.add("https://ihplb.b-cdn.net/wp-content/uploads/2018/06/Qutub-Minar.jpg");
+
+        imageName.add("Hawa Mahal");
+        imageUrl.add("https://ihplb.b-cdn.net/wp-content/uploads/2018/06/Hawa-Mahal.jpg");
+
+        imageName.add("Gateway of India");
+        imageUrl.add("https://ihplb.b-cdn.net/wp-content/uploads/2018/06/Gateway-of-India.jpg");
+
+        imageName.add("Charminar");
+        imageUrl.add("https://ihplb.b-cdn.net/wp-content/uploads/2018/06/Charminar.jpg");
+
+        imageName.add("Jallianwala Bagh");
+        imageUrl.add("https://ihplb.b-cdn.net/wp-content/uploads/2018/06/Jallianwala-Bagh.jpg");
+
+        imageName.add("Sikkim");
+        imageUrl.add("https://static-blog.treebo.com/wp-content/uploads/2018/01/Sikkim.jpg");
+
+    }
+
+    private void initRecyclerView(){
+        Log.d(TAG, "initRecyclerView: ");
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, imageName, imageUrl);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+}
---
+++ app/src/main/java/com/kellton/mvvmtest1/RecyclerViewAdapter.java
@@ -0,0 +1,80 @@
+package com.kellton.mvvmtest1;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private static final String TAG = "RecycleViewAdapter";
+
+    private Context mContext;
+    private ArrayList<String> mImageName = new ArrayList<>();
+    private ArrayList<String> mImages = new ArrayList<>();
+
+    public RecyclerViewAdapter(Context mContext, ArrayList<String> mImageName, ArrayList<String> mImages) {
+        this.mContext = mContext;
+        this.mImageName = mImageName;
+        this.mImages = mImages;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_list_item, parent, false);
+       ViewHolder holder = new ViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder()");
+
+        Glide.with(mContext)
+                .asBitmap()
+                .load(mImages.get(position))
+                .into(holder.imageView);
+        holder.tvCountryName.setText(mImageName.get(position));
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick()::");
+                Toast.makeText(mContext, mImageName.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageName.size();
+    }
+
+
+    class ViewHolder extends RecyclerView.ViewHolder{
+        RelativeLayout parentLayout;
+        CircleImageView imageView;
+        TextView tvCountryName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+            imageView = itemView.findViewById(R.id.image);
+            tvCountryName = itemView.findViewById(R.id.tv_country_name);
+        }
+    }
+}
